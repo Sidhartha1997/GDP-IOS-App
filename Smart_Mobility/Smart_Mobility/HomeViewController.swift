9d1b47ef9c2fa3991c00952d2a56ec915f85ee4d
eppalapellisatheesh <eppalapellisatheesh1@gmail.com>
2022-08-30 22:13:34 -0500

    added project from GDP-1

---
+++ Smart_Mobility/Smart_Mobility/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Smart_Mobility
+//
+//  Created by Dodla,Narayan Reddy on 5/23/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func Login(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func ForgetPassword(_ sender: UIButton) {
+        
+    }
+    
+    @IBAction func SignUp(_ sender: UIButton) {
+    
+    }
+    
+}
+
