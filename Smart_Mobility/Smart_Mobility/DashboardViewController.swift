e197e26eca66bfd4471db94181a312da72e319db
Satheesh Eppalapelli <eppalapellisatheesh1@gmail.com>
2022-09-24 16:05:59 -0500

    updated the pages and view

--- Smart_Mobility/Smart_Mobility/DashboardViewController.swift
+++ Smart_Mobility/Smart_Mobility/DashboardViewController.swift
@@ -5,10 +5,26 @@
 //  Created by Kondr Jitendra Chowdary on 5/24/22.
 //
 
+
 import UIKit
+import SwiftUI
 
 class DashboardViewController: UIViewController {
 
+    
+    var body: some View {
+        GeometryReader { geo in
+            ZStack{
+            Image("homebackground")
+                .resizable()
+                .scaledToFit()
+                .edgesIgnoringSafeArea(.all)
+                .frame(width: geo.size.width, height: geo.size.height, alignment: .center)
+                .opacity(1.0)
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
